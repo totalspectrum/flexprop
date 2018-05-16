e4408bfd4bbb6e5e73cc534d56ca05be8c5f0191
Eric Smith <ersmith@totalspectrum.ca>
2018-05-16 02:43:16 -0300

    still moving things around

---
+++ spin2gui.tcl
@@ -0,0 +1,16 @@
+#!/usr/bin/wish
+#
+# Simple GUI for Spin
+# Copyright 2018 Total Spectrum Software
+# Distributed under the terms of the MIT license;
+# see License.txt for details.
+#
+# Top level program
+
+package require Tk
+package require autoscroll
+#package require ctext
+
+source src/ctext/ctext.tcl
+source src/gui.tcl
+
