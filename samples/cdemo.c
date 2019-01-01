ee5e2cd7df8a8d90ea28e8401b52e2b31adfde2d
Eric Smith <ersmith@totalspectrum.ca>
2019-01-01 18:40:17 -0400

    add C demo

---
+++ samples/cdemo.c
@@ -0,0 +1,27 @@
+// P2 blinking lights and serial demo
+// this requires a P2-EVAL board
+
+#include <stdio.h>
+#include <propeller.h>
+
+#define FREQ 160000000
+#define OSCMODE 0x10c3f04
+#define BAUD 2000000
+
+#define PIN 58
+
+void main()
+{
+    unsigned int pinmask = 1<<(PIN-32);
+    unsigned i = 0;
+
+    clkset(OSCMODE, FREQ);
+    _setbaud(BAUD);
+    printf("fastspin C demo\n");
+    DIRB |= pinmask; // set pins as output
+    for(;;) {
+        OUTB ^= pinmask;
+        waitcnt(getcnt() + CLKFREQ/4);
+        printf("toggle %u\n", i++);
+    }
+}
