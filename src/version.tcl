853b4a3bc80b2faa79d80f7b50008841c1f2dc87
Eric Smith <ersmith@totalspectrum.ca>
2020-05-27 17:37:28 -0300

    prepare for a beta version

--- src/version.tcl
+++ src/version.tcl
@@ -8,12 +8,12 @@
 # 2 "<stdin>" 2
 
 set spin2gui_version_major 4
-set spin2gui_version_minor 1
-set spin2gui_version_rev 12
+set spin2gui_version_minor 2
+set spin2gui_version_rev 0
+
+set spin2gui_beta "-beta"
 
 
-
-set spin2gui_beta ""
 
 
 set spin2gui_version "$spin2gui_version_major.$spin2gui_version_minor.$spin2gui_version_rev$spin2gui_beta"
