330f4eaf779853b85d92d812a408de74b19f9ef4
Eric Smith <ersmith@totalspectrum.ca>
2020-10-18 14:43:59 -0300

    updated version

--- src/version.tcl
+++ src/version.tcl
@@ -8,12 +8,12 @@
 # 2 "<stdin>" 2
 
 set spin2gui_version_major 4
-set spin2gui_version_minor 4
-set spin2gui_version_rev 1
-
-set spin2gui_beta "-beta"
+set spin2gui_version_minor 5
+set spin2gui_version_rev 0
 
 
+
+set spin2gui_beta ""
 
 
 set spin2gui_version "$spin2gui_version_major.$spin2gui_version_minor.$spin2gui_version_rev$spin2gui_beta"
