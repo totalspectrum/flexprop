c9ba40face932fe708c55777ef59e05cffc64507
Eric Smith <ersmith@totalspectrum.ca>
2023-01-06 10:46:21 -0400

    simplify cdemo.c

--- samples/cdemo.c
+++ samples/cdemo.c
@@ -1,25 +1,17 @@
 // P2 blinking lights and serial demo
 // this requires a P2-EVAL board
 
+// to get the frequency we desire, define _clkfreq in an enum
+// to use a board with a different frequency, also define
+// _xtalfreq
+// do this *before* including propeller.h
+
+enum {
+    _clkfreq = 180'000'000,
+};
+
 #include <stdio.h>
 #include <propeller.h>
-
-// to get the frequency we desire, we
-// specify P2_TARGET_MHZ then include "sys/p2es_clock.h"
-// e.g. to get 160_000_000, define P2_TARGET_MHZ to 160
-// p2es_clock.h defines constants _SETFREQ and _CLOCKFREQ
-// which we can then use to set the clock
-//
-// note that this is not the only way to set the clock; you can
-// certainly calculate the desired clock mode based on the frequency
-// and pass the resulting mode and frequency directly to clkset()
-// p2es_clock.h is just a convenience header
-#define P2_TARGET_MHZ 160
-#include "sys/p2es_clock.h"
-
-#ifndef _BAUD
-#define _BAUD 230400
-#endif
 
 #define PIN 56
 
@@ -28,13 +20,10 @@ void main()
     unsigned int pinmask = 1<<(PIN-32);
     unsigned i = 0;
 
-    clkset(_SETFREQ, _CLOCKFREQ);
-    _setbaud(_BAUD);
-    printf("fastspin C demo: clockmode is $%x, clock frequency %u Hz\n", _SETFREQ, _CLOCKFREQ);
     DIRB |= pinmask; // set pins as output
     for(;;) {
         OUTB ^= pinmask;
-        waitcnt(getcnt() + _CLOCKFREQ/4);
+        waitcnt(getcnt() + _clkfreq/4);
         printf("Toggle %u\n", i++);
     }
 }
